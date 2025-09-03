2b290da9faa3231f4cdb2fd8fa1b3194c43da1b3
demo-git1905 <piusonawane512@gmail.com>
2025-09-03 16:01:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/bloggingDB", {useNewUrlParser: true});
+-e MONGO_URI=mongodb://host.docker.internal:27017/bloggingDB
 
 
 const postSchema = {
